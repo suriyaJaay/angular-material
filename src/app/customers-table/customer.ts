bfd23a5aede4144b4742fcf51ef55f719538ffc5
suriya <suriyaJaay@users.noreply.github.com>
2021-07-28 18:27:06 +0530

    Update customer.ts

--- src/app/customers-table/customer.ts
+++ src/app/customers-table/customer.ts
@@ -1,4 +1,18 @@
-export interface Customer {
+/* export interface Customer {
   name: string;
   age: number;
+} */
+
+
+export interface Customers {
+    name: string;
+    description: null | string;
+    obj: Obj;
+}
+
+export interface Obj {
+    CustId: string;
+    custName: string;    
+    country: string;
+    region: string;    
 }
